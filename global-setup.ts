73d5a9f332b58da0f5258aa347521eedd79cf115
refael-m <refael.mahpud@cloudinary.com>
2022-11-09 12:06:10 +0200

    update

--- global-setup.ts
+++ global-setup.ts
@@ -2,7 +2,7 @@ import { FullConfig } from '@playwright/test';
 import logger from './logger/logger';
 
 async function globalSetup(config: FullConfig) {
-    logger.info('in test - winston');
+    logger.info('in global setup - winston');
 }
 
 export default globalSetup;
